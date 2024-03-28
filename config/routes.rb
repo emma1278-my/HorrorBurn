71c6e475f38a6cd2bab66d49c9d125f744c39e5f
rq-emma1278-my-at-321899771753 <ellie.co729@gmail.com>
2024-03-28 14:24:37 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root "tops#index"
-
+  
+  resources :users, only: %i[new create]
   get 'top_index' , to: 'tops#index'
   get 'privacy_policy', to: 'tops#privacy_policy'
   get 'terms_of_service', to: 'tops#terms_of_service'
+
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
