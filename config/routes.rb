0b7a490d0f6d2e5cb0450febc7bcb891a9317f9e
rq-emma1278-my-at-321899771753 <ellie.co729@gmail.com>
2024-04-05 20:32:12 +0900

    パスワードリセット絹雄の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  root "tops#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development? 
   
+  root "tops#index"
+
   get 'top_index' , to: 'tops#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   get 'privacy_policy', to: 'tops#privacy_policy'
   get "guest_login", to: "users#guest_login"
-
+ 
    # 利用規約とプライバシーポリシー
   get 'terms_of_service', to: 'tops#terms_of_service'
-
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # 基礎代謝算出
   get 'metabolism_calculators/new', to: 'metabolism_calculators#new', as: 'new_metabolism_calculators'
   post 'metabolism_calculators/show', to: 'metabolism_calculators#show'
@@ -21,5 +23,6 @@ Rails.application.routes.draw do
   resource :profile, only: %i[show edit update]
   get '/users/withdraw', to: 'users#withdraw'
   patch  '/users/withdraw' => 'users#withdraw'
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_05_073427) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_05_091142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_05_073427) do
     t.string "avatar"
     t.boolean "is_deleted"
     t.boolean "guest", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
