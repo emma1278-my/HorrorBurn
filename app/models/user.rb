952cac198f4ec168c1be70ed7195174a7d5820cb
emma1278-my <ellie.co729@gmail.com>
2024-05-17 14:16:36 +0900

    マイグレーションファイル変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
  
   mount_uploader :avatar, AvatarUploader
   
-  validates :email, uniqueness: { scope: :is_deleted }, presence: true
+  validates :email, uniqueness: { scope: :deleted_at }, presence: true
   validates :name, presence: true, length: { maximum: 255 }
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :weight_logs, dependent: :destroy
    
   def soft_delete
-    update(is_deleted: Time.current)
+    update(deleted_at : Time.current)
   end
 
   def total_watched_runtime
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   private
 
   def active_for_authentication?
-    super && is_deleted.nil?
+    super && deleted_at.nil?
   end
 
     # ランダムなユーザーIDを生成
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_17_025315) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_051350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,12 +67,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_17_025315) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
-    t.boolean "is_deleted"
     t.float "current_weight"
     t.float "target_weight"
     t.float "remaining_weight"
     t.float "target_calorie"
     t.float "remaining_runtime"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
