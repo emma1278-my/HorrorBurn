3fba2a6a05a304b934c84899ba484ce586484115
emma1278-my <ellie.co729@gmail.com>
2024-05-24 11:26:11 +0900

    国際化

--- app/controllers/weight_logs_controller.rb
+++ app/controllers/weight_logs_controller.rb
@@ -14,23 +14,27 @@ class WeightLogsController < ApplicationController
 
   def create
     @weight_log = current_user.weight_logs.new(weight_params)
-    if @weight_log.save    
-      flash[:success] = t(".success")
-      # 最新の体重
-      latest_weight = @weight_log.weight
-      # 目標体重までの残り
-      target_weight = current_user.target_weight
-      @remaining_weight = latest_weight - target_weight
-      # 目標体重に到達したかチェック
-    if @remaining_weight <= 0
-      flash[:success] = t(".next_weight_log")
+    
+    if current_user.target_calorie.present?
+      if @weight_log.save
+        flash[:success] = t(".success")
+        # 最新の体重
+        latest_weight = @weight_log.weight
+        # 目標体重までの残り
+        target_weight = current_user.target_weight
+        @remaining_weight = latest_weight - target_weight
+        # 目標体重に到達したかチェック
+        if @remaining_weight <= 0
+          flash[:success] = t(".next_weight_log")
+        end
+        redirect_to dashboard_path(id: current_user.id)
+      end
+    else
+      flash[:alert] = t(".target_calorie_not_set")
+      redirect_to weight_logs_path
     end
-    redirect_to dashboard_path(id: current_user.id)
-  else
-    flash[:alert] = t(".failure")
-    redirect_to dashboard_path(id: current_user.id)
   end
-end
+
    
 
   def update
