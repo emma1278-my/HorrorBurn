6886b2c9178b3ee6c0dcc9ba64b6e72f4acab6c1
emma1278-my <ellie.co729@gmail.com>
2024-05-30 09:55:45 +0900

    パスワード再発行実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'horrorburn-1.onrender.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    domain:               'example.com',
+    user_name:            ENV['EMAIL_USER_NAME'],
+    password:             ENV['EMAIL_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true 
+    enable_starttls_auto: true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
