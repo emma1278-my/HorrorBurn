b241036e8dfff12be51da7c734be4046b17512f2
emma1278-my <ellie.co729@gmail.com>
2024-06-17 16:11:08 +0900

    profile変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,37 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "chartkick/chart.js"
-import { Controller } from "@hotwired/stimulus"
-
-
-// Connects to data-controller="autocomplete"
-export default class extends Controller {
-  static targets = ["input"]
-  connect() {
-    new Autocomplete(this.inputTarget, {
-      source: this.sourceValue,
-      onSelect: (item) => {
-        this.inputTarget.value = item
-      },
-    })
-  }
-
-  get sourceValue() {
-    return this.data.get("source")
-  }
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const file_button = document.querySelector('#avator-input');
-  const preview_img = document.querySelector('#preview');
-
-  file_button.addEventListener('change', (e) => {
-    let file = e.target.files;
-    let reader = new FileReader() ;
-    reader.readAsDataURL(file[0])
-    reader.onload = function() {
-      preview_img.src = reader.result;
-    }
-    },false);
-});
